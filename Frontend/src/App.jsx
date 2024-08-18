16e52e86f62272a1fd7ae5f2b6bbcec70006830b
Shivam Maisuriya <maisuriyas89@gmail.com>
2024-08-18 21:51:34 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+       <Navbar />
+       <Banner />
+    </>
+  )
+}
+
+export default App
